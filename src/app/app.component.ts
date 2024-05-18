9cf87606ffdd18401cfb63cd977dda621fc27f4f
Chaitra <chaitra.devadiga2003@gmail.com>
2024-05-18 23:22:39 +0530

    deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  constructor(
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    //this.apiService.getUser('johnpapa').subscribe(console.log);
+    //this.apiService.getRepos('johnpapa').subscribe(console.log)
+  }
+}
